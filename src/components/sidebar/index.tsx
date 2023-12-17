dc9b2f424093292554857700792b6d2f16487c60
valclassdevelop <developervalclass.com>
2023-12-17 13:53:31 +0700

    fix

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -4,6 +4,11 @@ import Image from 'next/image'
 import { FaArrowLeft, FaPlusCircle, FaSignOutAlt, FaWallet } from 'react-icons/fa'
 import '../../app/globals.css'
 import Link from 'next/link'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+import API from '@/services/api'
+import { isEqual, toRupiah } from '@/helpers'
+import { authSignIn } from '@/redux/authSlice'
 
 const Sidebar = ({ 
   type, show, router, onClick }: 
@@ -13,6 +18,23 @@ const Sidebar = ({
     router?: string, 
     onClick?: any 
   }) => {
+
+  const [dataUser, setDataUser] = useState<Record<string, any>>({})
+
+  const auth = useSelector((state: any) => state.authSlice.auth)
+  const dispatch = useDispatch()
+  console.log('auth', auth)
+
+  useEffect(() => {
+    (async () => {
+      const response = await API.getAccountById(auth?.user_id)
+      if(!isEqual(dataUser, response.data.data)) {
+        dispatch(authSignIn(response.data.data))
+        setDataUser(response.data.data)
+      }
+    })()
+  }, [dataUser, dispatch])
+
   switch(type) {
     case "auth": 
       return (
@@ -45,7 +67,7 @@ const Sidebar = ({
         <div className={`fixed flex ${show ? 'z-[99999999] left-[0%] shadow-lg' : 'left-[-100%] md:left-[0%] z-[9999999] '} md:flex bottom-0 w-[80vw] md:w-[26%] bg-blue-200 duration-200 flex item-center justify-center h-screen overflow-hidden`}>
           <div className='absolute flex items-center justify-between w-full px-5 top-5'>
             <div className='flex items-center w-max rounded-full text-[20px] top-8 bg-white text-blue-500 px-5 py-1 text-center'>
-              <p>Rp. 150.000</p>  
+              <p>{toRupiah(dataUser?.balance)}</p>  
             </div>
             <Link href={router ? router : '/'} className='z-40'>
               <div className='w-[50px] h-[50px] z-[111] p-2 rounded-full overflow-hidden bg-blue-400 flex items-center justify-center cursor-pointer hover:brightnes-[90%] active:scale-[0.96] text-white'>
